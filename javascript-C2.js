5acc2173204b4386ff3e332d2c996f0982892698
Yu Cheng Huang <cookiehuang310@gmail.com>
2025-05-11 15:02:44 -0700

    Updated

--- javascript-C2.js
+++ javascript-C2.js
@@ -22,7 +22,8 @@ function letterFinder(word, match) {
 letterFinder("test", "t");
 */
 
-//Error Prevention
+/*
+//Error Prevention, enabling running at the same time
 function addTwoNums(a,b){
     try{
         if (typeof a != 'number'){
@@ -39,7 +40,31 @@ function addTwoNums(a,b){
     }
 }
 
-
 addTwoNums(5,"5")
-console.log("It still workss")
+console.log("It still works")
+*/
 
+
+//Defensive Programming -> argument validation with condition vars
+function letterFinder(word, match) {
+    var condition1 = typeof(word) == 'string' && word.length >= 2
+    var condition2 = typeof(match) == 'string' && match.length == 1
+
+    if (condition1 && condition2)
+    {
+        for(var i = 0; i < word.length; i++) {
+        if(word[i] == match) {
+            //if the current character at position i in the word is equal to the match
+            console.log('Found the', match, 'at', i)
+        } else {
+            console.log('---No match found at', i)
+        }
+        }
+    } else {
+        console.log("Please pass correct arguments to the function.")
+    }
+}
+
+letterFinder(5,5)
+console.log()
+letterFinder("cat","c")
