2969bc121ea062653a7e45bd415b6eb22153acfe
Yu Cheng Huang <cookiehuang310@gmail.com>
2025-05-17 11:52:27 -0700

    Updated

--- javascript-C2.js
+++ javascript-C2.js
@@ -213,3 +213,47 @@ console.log(greet + " " + place + "!"); //display both variables without using t
 console.log(`${greet} ${place} !`) //display both variables using template literals
 
 console.log(`${1 + 1 + 1 + 1 + 1} stars!`) //it's possible to perform arithmetic operation inside a template literal expression
+
+//Spread
+//join arrays
+
+const fruits = ['apple', 'pear', 'plum'];
+const berries = ['blueberry', 'strawberry'];
+const fruitsAndBerries = [...fruits, ...berries]; // concatenate
+console.log(fruitsAndBerries); // ['apple', 'pear', 'plum', 'blueberry', 'strawberry'];
+
+//join objects
+
+const flying = { wings: 2 };
+const car = { wheels: 4 };
+const flyingCar = {...flying, ...car};
+console.log(flyingCar); // {wings: 2, wheels: 4}
+
+//ADD new members to arrays without using the push() method
+
+let veggies = ['onion', 'parsley'];
+veggies = [...veggies, 'carrot', 'beetroot'];
+console.log(veggies); // ['onion', 'parsley', 'carrot', 'beetroot'];
+
+
+//CONVERT a string to an array using the spread operator
+
+const greeting = "Hello";
+const arrayOfChars = [...greeting];
+console.log(arrayOfChars); //  ['H', 'e', 'l', 'l', 'o']
+
+//COPY either an object or an array into a separate one
+
+const car1 = {
+    speed: 200,
+    color: 'yellow'
+}
+const car2 = {...car1};
+car1.speed = 201;
+console.log(car1.speed, car2.speed); // 201, 200
+
+
+const fruits1 = ['apples', 'pears'];
+const fruits2 = [...fruits1];
+fruits1.pop();
+console.log(fruits1, "not", fruits2); // ['apples'] 'not' ['apples','pears'];
