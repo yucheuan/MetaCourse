df63c9d260905b4091ab550f9a7dfd79bf1612d7
Yu Cheng Huang <cookiehuang310@gmail.com>
2025-05-18 12:23:27 -0700

    DOM

--- javascript-C2.js
+++ javascript-C2.js
@@ -257,3 +257,24 @@ const fruits1 = ['apples', 'pears'];
 const fruits2 = [...fruits1];
 fruits1.pop();
 console.log(fruits1, "not", fruits2); // ['apples'] 'not' ['apples','pears'];
+
+//DOM
+//Prompt
+let answer = prompt('What is your name?');
+if (typeof(answer) === 'string') {
+    var h1 = document.createElement('h1')
+    h1.innerText = answer;
+    document.body.innerText = '';
+    document.body.appendChild(h1);
+}
+
+//HTML attribute
+var h1 = document.createElement('h1')
+h1.innerText = "Type into the input to make this text change"
+
+var input = document.createElement('input')
+input.setAttribute('type', 'text')
+
+document.body.innerText = '';
+document.body.appendChild(h1);
+document.body.appendChild(input);
