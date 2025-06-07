5903d0ad822574e42a2617b45a2ec14ea79ee8ed
yucheuan <cookiehuang310@gmail.com>
2025-06-07 14:18:07 -0700

    update

--- react-app-C5/src/App.js
+++ react-app-C5/src/App.js
@@ -1,25 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 
+//functional component Header in App file
+function Header() {
+  return <h1>Hello world</h1>
+}
+
+//functional component App in App file
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Header />
+  )
 }
 
 export default App;
