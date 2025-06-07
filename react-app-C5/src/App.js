46bb1ee79759e073c59368ca8ff71fc456404db7
yucheuan <cookiehuang310@gmail.com>
2025-06-07 16:45:36 -0700

    update file structure

--- react-app-C5/src/App.js
+++ react-app-C5/src/App.js
@@ -1,17 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-
-//functional component Header in App file
-function Header() {
-  return <h1>Hello world</h1>
-}
+import Heading from "./components/Heading"
 
 //functional component App in App file
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Header />  
+        <Heading />  
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
---
+++ react-app-C5/src/components/Heading.js
@@ -0,0 +1,4 @@
+//functional component Heading in Heading file
+export default function Heading() {
+  return <h1>Hello world 😃</h1>
+}
