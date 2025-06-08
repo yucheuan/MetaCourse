01df8d696300695686f7c3bc043aad1ba017b9b6
yucheuan <cookiehuang310@gmail.com>
2025-06-08 16:56:45 -0700

    update

--- react-app-C5/src/components/Intro.js
+++ react-app-C5/src/components/Intro.js
@@ -2,7 +2,7 @@ export default function Intro(props) {
     return (
         <div>
             <h2>
-                The value of the toggleBoolean prop is:{props.toggleBoolean.toString()}
+                The value of the toggleBoolean prop is: {props.toggleBoolean.toString()}
             </h2>
             <p>The value of the math prop is: <em>{props.math}</em></p>
             <p>The value of the str prop is: <em>{props.str}</em></p>
