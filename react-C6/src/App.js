3822c2b36837f954f8d275922378c64184ed88da
yucheuan <cookiehuang310@gmail.com>
2025-06-17 21:28:56 -0700

    update

--- react-C6/src/App.js
+++ react-C6/src/App.js
@@ -1,6 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
 
+const data = [
+  {
+    id: '1',
+    title: 'Tiramisu',
+    description: 'The best tiramisu in town',
+    price: '$5.00',
+  },
+  {
+    id: '2',
+    title: 'Chocolate mousse',
+    description: 'Unexplored flavour',
+    price: '$6.00',
+  },
+];
+
+const topDeseserts = data.map(dessert => {
+  return{
+    content: `${dessert.title} - ${dessert.description}`,
+    price: dessert.price,
+  }
+})
+
 function App() {
   return (
     <div className="App">
