1e51f08637cdf2f3336848f3750a7d77bc86975a
yucheuan <cookiehuang310@gmail.com>
2025-06-19 21:37:50 -0700

    feedback form

--- react-C6/src/Form.js
+++ react-C6/src/Form.js
@@ -2,11 +2,20 @@ import { useState } from "react"
 
 export default function Form (){
     const [name, setName] = useState("")
+    const [score, setScore] = useState("10")
+    const [comment, setComment] = useState("")
 
     const handleSubmit = (e) => {
         e.preventDefault()
+        if (Number(score) <= 5 && comment.length <= 10) {
+            alert("PLease provide a comment explaining why the experience was poor")
+            return
+        }
+
         setName("")
-        console.log("FOrm Submitted!")
+        setScore("10")
+        setComment("")
+        console.log("Form Submitted!")
     }
 
     return(
@@ -22,6 +31,20 @@ export default function Form (){
                         value={name}
                         onChange={(e) => setName(e.target.value)} 
                     />
+                </div>
+                <div>
+                    <label>Score: {score}</label>
+                    <input
+                        type="range"
+                        min="0"
+                        max="10"
+                        value={score}
+                        onChange={e => setScore(e.target.value)} 
+                    />
+                </div>
+                <div>
+                    <lable>Comment:</lable>
+                    <textarea value={comment} onChange={e => setComment(e.target.value)}/>
                 </div>
                 <button disabled={!name} type="submit">
                     Submit
