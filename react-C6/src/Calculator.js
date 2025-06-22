37d380edf1c7c6914e951afbb34380f6b8b3c153
yucheuan <cookiehuang310@gmail.com>
2025-06-22 10:37:55 -0700

    update

--- react-C6/src/Calculator.js
+++ react-C6/src/Calculator.js
@@ -3,7 +3,7 @@ import { useReducer } from "react";
 const reducer = (state, action) => {
     if (action.type === 'spend_$') return {money: state.money - 10}
     if (action.type === 'earn_$') return {money: state.money +10}
-    return state
+    return state //wrong_type -> do nothing (use previous state, instead of undefined)
 }
 
 export default function Calculator (){
