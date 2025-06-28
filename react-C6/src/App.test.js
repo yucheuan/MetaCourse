8d412d725435405e560029e4ca6485dc6f93607d
yucheuan <cookiehuang310@gmail.com>
2025-06-28 15:29:17 -0700

    Jest test

--- react-C6/src/App.test.js
+++ react-C6/src/App.test.js
@@ -1,8 +1,13 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { ThemeProvider } from './ThemeContext';
 
 test('renders Mouse position', () => {
-  render(<App />);
-  const linkElement = screen.getByText("Mouse position");
+  render(
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
+  );
+  const linkElement = screen.getByText("Mouse position:");
   expect(linkElement).toBeInTheDocument();
 });
